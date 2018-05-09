9b08605812e60ba679b59dca8be06b608c686f54
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-09 20:13:39 +0100

    Cleaned up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
+import api_key from './Components/api-key'
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      api_key: api_key
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        
       </div>
     );
   }
