c6d04a6196856e977f15c29bf771e094968bba41
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-24 18:34:43 +0100

    Added/Styled Header image

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const Header = () => <h1>Header</h1>
+const Header = () => {
+  return(
+    <div className="header">
+      <div className="container">
+        <img src={require("../Images/Logo.png")} alt="Clash Royale Logo"/>
+      </div>
+    </div>
+  )
+}
 
 export default Header
