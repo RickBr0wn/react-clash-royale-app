2a4ea618f49b70e903c47bb6dd45aa3b009170e6
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-30 23:43:58 +0100

    Corrected CSS

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Stats = props =>{
+  const { name, trophies, arena } = props.data
+  console.log(arena.name)
   return(
-    <div className="component-wrapper">
+    <div>
       <h2>Player Information</h2>
+      <div className="player-information">
+        <ul>
+          <li>Name: {name}</li>
+          <li>Trophies: {trophies}</li>
+          <li>Arena: {arena.arenaID}</li>
+          <li>Arena Name: {arena.name}</li>
+          <li>Min trophies: {arena.trophyLimit}</li>
+        </ul>
+      </div>
     </div>
   )
 }
